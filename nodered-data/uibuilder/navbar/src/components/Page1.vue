d72afdfd523f0eb05d88e170ba1c2898a391aca4
acraf <aacraaf@gmail.com>
2022-04-27 13:11:08 +0200

    mlopsv1

--- nodered-data/uibuilder/navbar/src/components/Page1.vue
+++ nodered-data/uibuilder/navbar/src/components/Page1.vue
@@ -2,7 +2,12 @@
   <b-container fluid class="pr-5">
     <h2 class="ml-4 mt-4">Page1</h2>
     <vue-json-pretty :deep="0" :showLength="true" :showDoubleQuotes="false" :showSelectController="true" :deepCollapseChildren="true" :data="msg" > </vue-json-pretty>
-  </b-container>
+    <ul id="example-1">
+      <li v-for="medicion in msg.payload">
+        {{ medicion.measurement }}
+      </li>
+    </ul>
+    </b-container>
 </template>
 
 <script>
